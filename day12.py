4587ca88358a1a733f3eee2bd4f7f497b9ad3958
heegyun <yhg1124@daum.net>
2022-07-19 12:30:02 +0900

    Add files via upload

---
+++ day12.py
@@ -0,0 +1,22 @@
+# import theater_module
+
+# theater_module.price(3)
+# theater_module.price_morning(4)
+# theater_module.price_solder(5)
+
+# import theater_module as mv
+# mv.price(3)
+# mv.price_morning(4)
+# mv.price_solder(5)
+
+# from theater_module import *
+# price(3)
+# price_morning(4)
+# price_solder(4)
+
+# from theater_module import price, price_morning
+# price(5)
+# price_morning(4)
+
+from theater_module import price_solder as price
+price(5)
